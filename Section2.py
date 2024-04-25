8908c8d997cb6eb9e2edca5010c387eca9604503
Mutiu Adegboye <adegboyemutiu@gmail.com>
2024-04-26 00:58:50 +0100

    FEA read video

--- Section2.py
+++ Section2.py
@@ -1,8 +1,20 @@
 import cv2
 print("Package Imported")
 
+"""""
+#********************* IMPORT IMAGE ********************
 
 img = cv2.imread("Images/lenna.png")
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)
+"""""
+#********************* IMPORT VIDEO ********************
+
+VideoCap = cv2.VideoCapture("Videos/dog.mp4")
+while True:
+    successful, img = VideoCap.read()
+    cv2.imshow("video", img)
+
+    if cv2.waitKey(1) & 0XFF ==ord('q'):
+        break

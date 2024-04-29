05bd950db37608d882db43a2520785329c9a6a6e
Mutiu Adegboye <adegboyemutiu@gmail.com>
2024-04-30 00:53:23 +0100

    FEA perform image blurring

--- Basic_OpenCVC_functions_CV.py
+++ Basic_OpenCVC_functions_CV.py
@@ -3,13 +3,121 @@ import cv2
 
 #load image
 img = cv2.imread("Images/image021.jpg")
+cv2.imwrite("Results/RGB_images.jpg", img)
 
 #**************** convert the image to grayscale ********************
 imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-# Display grayscale imagecd 
+# Display grayscale imagecd
 cv2.imshow("Grayscale image", imgGray)
-
-#cv2.waitKey(0)
 cv2.imwrite("Results/GraySca_image.jpg", imgGray)
 
+
+#**************** Image Blurring ********************
+imgBlurred = cv2.GaussianBlur(img, (11, 11), 0)
+
+# Display the blurred image
+cv2.imshow("Blurred image", imgBlurred)
+# Save the reuslt
+cv2.imwrite("Results/Blurred_image.jpg", imgBlurred)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()

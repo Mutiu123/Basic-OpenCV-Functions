94cd46eb326d31eb646dc721e2d4662f6313e68b
Mutiu Adegboye <adegboyemutiu@gmail.com>
2024-05-14 01:22:52 +0100

    FEA add image stacking function

---
+++ multipleImageMerger.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+
+
+#perform iamges stack
+def imagesStack(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+#load image
+img = cv2.imread("Images/image021.jpg")
+imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+
+# Apply canny edge detection on grayscale image
+
+CamyEdges = cv2.Canny(imgGray, 30, 100)
+
+imagesStack = imagesStack(0.35,([img,img,CamyEdges,img],[img,imgGray,img,img]))
+
+
+# Display equalised image
+cv2.imshow("Merged Image", imagesStack)
+
+# Save the reuslt
+cv2.imwrite("Results/StackImages.jpg", imagesStack)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
